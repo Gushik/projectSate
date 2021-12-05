dca42d0f09e83db77dbc36b51929c394093e05a5
Yura2021 <79528600+Yura2021@users.noreply.github.com>
2021-12-05 13:13:08 +0200

    Route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageMainComponent } from './page-main/page-main.component';
+import { PageNotNoundComponent } from './page-not-nound/page-not-nound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/page-main',pathMatch:'full'},
+ {path:'page-main',component:PageMainComponent},
+  {path:'**',component:PageNotNoundComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
