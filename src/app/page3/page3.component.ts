ca178838177139c1f7af51fabbafac28d433f48d
Gushik <gushik1986@gmail.com>
2021-12-10 01:22:03 +0200

    anime3

--- src/app/page3/page3.component.ts
+++ src/app/page3/page3.component.ts
@@ -1,4 +1,4 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -12,11 +12,40 @@ import { Component, OnInit } from '@angular/core';
         animate('1.2s', style({ opacity: 1 })),
       ]),
     ]),
+    trigger('moveMouseDiv', [
+      state('start', style({
+        height:'*',
+        
+
+      })),
+      state('end', style({
+        height:'450px',
+        position: 'relative',
+        top: '-560px',
+    left: '75.5%',
+        zIndex: '1',
+
+      })),
+      
+      transition('start => end', animate (500)),
+      transition('end => start', animate ('500ms 0.5s ease-out'))
+    ]),
   ],
 
 })
 export class Page3Component implements OnInit {
 
+  moveDivState = 'start';
+
+  changeMoveDivState(): void{
+    this.moveDivState = 'end';
+    
+
+  }
+  changeOutDivState(): void{
+    this.moveDivState = 'start';
+
+  }
   constructor() { }
 
   ngOnInit(): void {
