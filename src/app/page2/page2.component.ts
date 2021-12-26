4cbe38f335e93039c7ebfea10fc8c860eb71732d
Gushik <gushik1986@gmail.com>
2021-12-26 14:57:36 +0200

    project_cursova

--- src/app/page2/page2.component.ts
+++ src/app/page2/page2.component.ts
@@ -1,6 +1,9 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
-
+export interface CardData {
+  imageId: string;
+  state: 'default' | 'flipped' | 'matched';
+}
 @Component({
   selector: 'app-page2',
   templateUrl: './page2.component.html',
@@ -25,10 +28,10 @@ import { Component, OnInit } from '@angular/core';
 
       })),
       state('end', style({
-        height:'1250px',
-        position: 'relative',
-        top: '560px',
-    left: '72.5%',
+        width: "60%", 
+        position: 'absolute',
+        top: '60px',
+        left: '30%',
         zIndex: '1',
 
       })),
@@ -36,6 +39,26 @@ import { Component, OnInit } from '@angular/core';
       transition('start => end', animate (500)),
       transition('end => start', animate ('500ms 0.5s ease-out'))
     ]),
+    trigger('move', [
+      state('in', style({ opacity: 0 })),
+      state('out', style({ opacity: 1 })),
+      transition('in => out', animate('4s linear')),
+      transition('out => in', animate('4s linear'))
+    ]),
+    trigger('cardFlip', [
+      state('default', style({
+        transform: 'none'
+      })),
+      state('flipped', style({
+        transform: 'rotateY(360deg)'
+      })),
+      transition('default => flipped', [
+        animate('1500ms')
+      ]),
+      transition('flipped => default', [
+        animate('1500ms')
+      ])
+    ])
   ],
 
 })
@@ -50,6 +73,31 @@ export class Page2Component implements OnInit {
   changeOutDivState(): void{
     this.moveDivState = 'start';
 
+  }
+  state = 'in';
+  ngAfterViewInit() {
+    setTimeout(() => {
+      this.state = 'out';
+    }, 0);
+  }
+  onEnd(event:any) {
+    this.state = 'in';
+    if (event.toState === 'in') {
+      setTimeout(() => {
+        this.state = 'out';
+      }, 0);
+    }
+  }
+  data: CardData = {
+    imageId: "pDGNBK9A0sk",
+    state: "default"
+  };
+  cardClicked() {
+    if (this.data.state === "default") {
+      this.data.state = "flipped";
+    } else {
+      this.data.state = "default";
+    }
   }
   constructor() { }
 
